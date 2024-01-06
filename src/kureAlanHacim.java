e4a584a7cc0ffc04681a2df87aff53636a654caa
turan <turanaymis@gmail.com>
2024-01-06 22:51:13 +0300

    Küre alan hacim hesabı

---
+++ src/kureAlanHacim.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class kureAlanHacim {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double r,alan,hacim=0;
+        double pi = 3.14;
+
+        System.out.println("Dairenin yarı çapını giriniz: ");
+        r = scan.nextDouble();
+
+        alan = 4*pi*r*r;
+        hacim = (4/3)*pi*r*r*r;
+
+        System.out.println("Alan: "+alan);
+        System.out.println("Hacim: "+hacim);
+
+    }
+}
